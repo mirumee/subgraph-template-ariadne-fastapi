9bd09507ea05081d62ff5d67fd96f43a4b10b23c
Patrick Arminio <patrick.arminio@gmail.com>
2022-09-12 09:50:42 +0100

    Migrate to ariadne

--- api/schema.py
+++ api/schema.py
@@ -1,52 +1,20 @@
-from typing import Optional
-import strawberry
+from pathlib import Path
 
-from strawberry.schema_directive import Location
-
-
-@strawberry.schema_directive(locations=[Location.SCHEMA])
-class Contact:
-    name: str = strawberry.field(description="Contact title of the subgraph owner")
-    url: Optional[str] = strawberry.field(
-        description="URL where the subgraph's owner can be reached"
-    )
-    description: Optional[str] = strawberry.field(
-        description="Other relevant notes can be included here; supports markdown links"
-    )
+from ariadne import load_schema_from_path, ObjectType
+from ariadne.contrib.federation import make_federated_schema
 
 
-@strawberry.federation.type(keys=["id"])
-class Foo:
-    id: strawberry.ID
-    name: Optional[str]
-
-    @classmethod
-    def resolve_reference(cls, representation: dict) -> "Foo":
-        return cls(id=strawberry.ID("1"), name="Foo")
+HERE = Path(__file__).parent
 
 
-@strawberry.type
-class Query:
-    @strawberry.field
-    def foo(self, id: strawberry.ID) -> Optional[Foo]:
-        return Foo(id=id, name="Foo")
+query = ObjectType("Query")
 
 
-@strawberry.type
-class Mutation:
-    @strawberry.mutation
-    def create_user(self, name: str) -> str:
-        return name
+@query.field("foo")
+def resolve_foo(*_, id: str):
+    return {"id": id, "name": "Foo"}
 
 
-schema = strawberry.federation.Schema(
-    Query,
-    Mutation,
-    schema_directives=[
-        Contact(
-            name="FooBar Server Team",
-            url="https://myteam.slack.com/archives/teams-chat-room-url",
-            description="send urgent issues to [#oncall](https://yourteam.slack.com/archives/oncall).",
-        )
-    ],
-)
+type_defs = load_schema_from_path(str(HERE / "schema.graphql"))
+
+schema = make_federated_schema(type_defs, query)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from ariadne.asgi import GraphQL
 from fastapi import FastAPI
-from strawberry.fastapi import GraphQLRouter
 
 from api.schema import schema
 
-graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
-app.include_router(graphql_app, prefix="/graphql")
+app.add_route("/graphql", GraphQL(schema))

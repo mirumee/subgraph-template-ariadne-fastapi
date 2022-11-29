660bb78cbad9550ddcb7f4b64dffcc660530299f
Patrick Arminio <patrick.arminio@gmail.com>
2022-11-29 15:59:02 +0000

    Add CORS configuration

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from ariadne.asgi import GraphQL
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.schema import schema
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://studio.apollographql.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.add_route("/", GraphQL(schema))
